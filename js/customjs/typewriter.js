4e6763ff2636c63ca6aef8139cb368dc91cf9fb2
ominezX9 <winstreakdavid@gmail.con>
2024-07-21 21:14:06 +0100

    update: add more assets ...

---
+++ js/customjs/typewriter.js
@@ -0,0 +1,55 @@
+const taglines = [
+    "Digital",
+    "Innovative",
+    "Modern",
+    "Contactless",
+    "Fast",
+    "Networking",
+    "Seamless",
+    "Efficient",
+    "Smart",
+    "Trusted",
+    "African",
+    "Nigerian",
+    "Business",
+    "Connection",
+    "Professional",
+    "Advanced",
+    "Convenient",
+    "Leading",
+    "Versatile",
+    "The Future"
+];
+
+const typingSpeed = 100; 
+const pauseTime = 1000; 
+const backspaceSpeed = 50;
+
+let index = 0;
+let charIndex = 0;
+const $typewriterContainer = $('#typewriter-container');
+
+function type() {
+    if (charIndex < taglines[index].length) {
+        $typewriterContainer.text($typewriterContainer.text() + taglines[index].charAt(charIndex));
+        charIndex++;
+        setTimeout(type, typingSpeed);
+    } else {
+        setTimeout(backspace, pauseTime);
+    }
+}
+
+function backspace() {
+    if (charIndex > 0) {
+        $typewriterContainer.text(taglines[index].substring(0, charIndex - 1));
+        charIndex--;
+        setTimeout(backspace, backspaceSpeed);
+    } else {
+        index = (index + 1) % taglines.length;
+        setTimeout(type, typingSpeed);
+    }
+}
+
+$(document).ready(function () {
+    type();
+});
